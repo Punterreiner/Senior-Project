b492ee94f1d1a612b51e73e77538e987873e932e
Punterreiner <77292865+Punterreiner@users.noreply.github.com>
2022-10-07 20:04:53 -0400

    button update

--- EAEC/EACalculator/models.py
+++ EAEC/EACalculator/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+levels = (
+    ('poor','POOR'),
+    ('ok', 'OK'),
+    ('good','GOOD'),
+    ('excellent','EXCELLENT')
+)
+
+class Fields(models.Model):
+    age = models.CharField(max_length=50)
+    spare_money = models.CharField(max_length=50)
+    free_time = models.CharField(max_length=50)
+    fitness = models.CharField(max_length=50, choices=levels, default='poor')
